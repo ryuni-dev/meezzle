21522f90618e261c70da830a72e4ee07cd6be45b
KUcodemaster <jh9485@korea.ac.kr>
2022-10-13 09:52:11 +0900

    Add Globalstyle.ts

--- meezzle-front/pages/index.tsx
+++ meezzle-front/pages/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { NextPage } from "next";
+import { GlobalStyle } from "../styles/Globalstyle";
 
 const Home: NextPage = () => {
-  return (
-    null
-  )
-}
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Hello world</h1>
+        </>
+    );
+};
 
-export default Home
+export default Home;
