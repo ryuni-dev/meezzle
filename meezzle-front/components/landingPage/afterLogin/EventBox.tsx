406d838349689e6d1ee1a5da1005e653449a5696
KUcodemaster <jh9485@korea.ac.kr>
2022-11-05 14:35:00 +0900

    로그인 후 화면 퍼블리싱

--- meezzle-front/components/landingPage/afterLogin/EventBox.tsx
+++ meezzle-front/components/landingPage/afterLogin/EventBox.tsx
@@ -1,13 +1,91 @@
 import styled from "styled-components";
 import { NextComponentType } from "next";
+import Image from "next/image";
+import user from "../../../public/assets/user.svg";
+import share from "../../../public/assets/share.svg";
 
-export const EventBox: NextComponentType = () => {
-  return <EventContainer></EventContainer>;
+interface Props {
+    userNum: number;
+    title: string;
+    idx: number;
+}
+
+interface ContainerProps {
+    backgroundColor: string;
+}
+
+export const EventBox = ({ userNum, idx, title }: Props) => {
+    const backgroundColors: string[] = [
+        "#FFE86D",
+        "#A1EAD8",
+        "#FFBDBD",
+        "#8AD4FD",
+        "#BEA5F3",
+    ];
+
+    const backgroundColor = backgroundColors[idx % 5];
+    return (
+        <EventContainer backgroundColor={backgroundColor}>
+            <IconContainer>
+                <Image src={user} alt="user" />
+                {userNum}
+                <Image src={share} alt="share" />
+            </IconContainer>
+            <Clear />
+            <TitleContainer>{title}</TitleContainer>
+            <DueContainer>
+                마감 <DueDate>22-09-30 23:59</DueDate>
+            </DueContainer>
+        </EventContainer>
+    );
 };
+const EventContainer = styled.div<ContainerProps>`
+    height: 121px;
 
-const EventContainer = styled.div`
-  height: 121px;
+    background: ${(props) => props.backgroundColor};
+    border-radius: 15px;
+    margin-bottom: 8px;
+`;
 
-  background: #ffe86d;
-  border-radius: 15px;
+const IconContainer = styled.div`
+    padding-right: 15px;
+    padding-top: 15px;
+    display: inline-flex;
+    & {
+        float: right;
+    }
+    span:last-child {
+        display: block;
+        margin-left: 10px !important;
+    }
+`;
+
+const Clear = styled.div`
+    display: table;
+    content: "";
+    clear: both;
+`;
+
+const TitleContainer = styled.h3`
+    margin-top: 17px;
+    margin-left: 27px;
+    font-family: "Pretendard";
+    font-weight: 700;
+    font-size: 18px;
+    margin-bottom: 2px;
+`;
+
+const DueContainer = styled.div`
+    font-family: "Pretendard";
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 150%;
+    margin-left: 27px;
+`;
+
+const DueDate = styled.p`
+    margin: 0;
+    display: inline-block;
+    font-weight: 400;
+    font-size: 12px;
 `;
