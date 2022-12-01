2fde1214908e841ce8bcef65987e84189603115f
ryuni-dev <ryuni.dev@gmail.com>
2022-12-02 00:16:06 +0900

    Decode Time 작성

--- meezzle-front/utils/converter.tsx
+++ meezzle-front/utils/converter.tsx
@@ -13,7 +13,7 @@ interface DataProps {
     participleTimes: string,
 }
 
-const testData = [
+const serverData = [
     {
         ableDayOfWeeks: 'MONDAY',
         participleTimes: '13:00-15:00,17:30-21:30',
@@ -28,7 +28,14 @@ const testData = [
     }
 ]
 
-// Generic 관련 수정 필요
+const clientData = [
+    134, 135, 136, 137, 138, 139,
+    140, 141, 142, 534, 535, 536,
+    537, 538, 539, 540, 541, 542,
+    618, 619, 620, 621, 622, 623,
+    624
+]
+
 export const ConvertDays4Client = (data:DataProps[]) => {
     let selectedTimes:number[] = [];
     const length = data[0].ableDayOfWeeks.length;
@@ -41,6 +48,17 @@ export const ConvertDays4Client = (data:DataProps[]) => {
     }
     return Array.from(new Set(selectedTimes));
 } 
+
+export const ConvertDays4Server = (data:number[]) => {
+    const d = Array.from(new Set(data));
+    const retData:DataProps[] = [];
+    d.sort();
+    for(let i = 0; i < d.length; i++){
+        const weekIdx = Math.trunc(d[i] / 100);
+        retData[i].ableDayOfWeeks = week[weekIdx];
+
+    }
+}
 
 const ChangeTime4Client = (participleTimes: string, day: number): number[] => {
     const timeArr = [];
@@ -55,6 +73,26 @@ const ChangeTime4Client = (participleTimes: string, day: number): number[] => {
         }
     }
     return timeArr;
+}
+
+const DecodeTime = (time: number) => {
+    if(time < 0 || time > 47){
+        console.log('DecodeTime Func Error : time range is wrong')
+    }
+    else {
+        const decode = ((time * 30) / 60).toFixed(1);
+        const hour = decode.split('.')[0];
+        if(decode.split('.')[1] === '0'){
+            return ('00'+hour).slice(-2) + ':00';
+        }
+        else {
+            return ('00'+hour).slice(-2) + ':30';
+        }
+    }
+}
+
+const ChangeTime4Server = (selectedTimes: number[]) => {
+
 }
 
 
