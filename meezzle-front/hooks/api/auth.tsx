25bbc99480f75700fcbe2f78ba725bc4a9a41b65
KUcodemaster <jh9485@korea.ac.kr>
2023-01-01 19:55:01 +0900

    build 오류 수정

--- meezzle-front/hooks/api/auth.tsx
+++ meezzle-front/hooks/api/auth.tsx
@@ -5,12 +5,3 @@ import { useLogin } from "../../states/login";
 export const useTest = () => {
     return useQuery(["auth"], () => api.getAuth());
 };
-
-export const useLogout = () => {
-    return useMutation(["logout"], () => api.logout(), {
-        onSuccess: () => {
-            const [loginState, setLoginState] = useLogin();
-            setLoginState(false);
-        },
-    });
-};
