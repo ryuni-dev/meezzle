1a28c73fa8c6430d430f63c59ff2683e5e579013
KUcodemaster <jh9485@korea.ac.kr>
2023-01-04 12:12:36 +0900

    prettier가 바꿈

--- meezzle-front/hooks/api/events.tsx
+++ meezzle-front/hooks/api/events.tsx
@@ -1,23 +1,27 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
-import * as api from '../../api/event';
-import { Events } from '../../types/EventProps';
+import { useMutation, useQuery } from "@tanstack/react-query";
+import * as api from "../../api/event";
+import { Events } from "../../types/EventProps";
 
 export const useEvent = (eid: string) => {
-    return useQuery(['event', eid], () => api.getEvent(eid));
-}
+    return useQuery(["event", eid], () => api.getEvent(eid));
+};
 
 // export const useEvents = () => {
 //     return useQuery(['event'], () => api.getEvents());
 // }
 
 export const useEvents_test = () => {
-    return useQuery(['event'], () => api.getEvents_test());
-}
+    return useQuery(["event"], () => api.getEvents_test());
+};
 
 export const useEventCreate_test = () => {
-    return useMutation(['eventCreate'], (data: string) => api.postCreate_test(data));
-}
+    return useMutation(["eventCreate"], (data: string) =>
+        api.postCreate_test(data)
+    );
+};
 
 export const useEventDelete = () => {
-    return useMutation(['eventDelete'], (uuid: string) => api.deleteEvent(uuid));
-}
+    return useMutation(["eventDelete"], (uuid: string) =>
+        api.deleteEvent(uuid)
+    );
+};
