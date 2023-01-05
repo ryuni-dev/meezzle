3a3ccaebe754b0842a56566690a6fe8e3c8b137a
ryuni-dev <ryuni.dev@gmail.com>
2023-01-05 20:19:17 +0900

    recoil type 수정

--- meezzle-front/states/eventDayBox.tsx
+++ meezzle-front/states/eventDayBox.tsx
@@ -1,12 +1,12 @@
 import { atom  } from "recoil";
 import { v1 } from "uuid";
 
-export const eventDaySelected = atom({
+export const eventDaySelected = atom<number[]>({
     key: `EventDayClick/${v1()}`,
     default: [],
 });
 
-export const eventDayCurrent = atom({
+export const eventDayCurrent = atom<number[]>({
     key: `EventDayCurrent/${v1()}`,
     default: [],
 });
