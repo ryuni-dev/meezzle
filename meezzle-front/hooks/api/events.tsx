30ddb82c9b1d2ab8ed713beaf83a8768c0426776
ryuni-dev <ryuni.dev@gmail.com>
2023-01-07 17:43:09 +0900

    수정, 투표 api 추가

--- meezzle-front/hooks/api/events.tsx
+++ meezzle-front/hooks/api/events.tsx
@@ -20,7 +20,25 @@ export const useEventCreate_test = () => {
 };
 
 export const useEventDelete = () => {
-    return useMutation(["eventDelete"], (uuid: string) =>
-        api.deleteEvent(uuid)
+    return useMutation(["eventDelete"], (eventId: string) =>
+        api.deleteEvent(eventId)
+    );
+};
+
+export const useEventPatch = (eventId: string) => {
+    return useMutation(["eventPatch"], (data:string) =>
+        api.patchEvent(data, eventId)
+    );
+};
+
+export const useEventVote4User = (eventId: string) => {
+    return useMutation(["eventVote-user"], (data:string) =>
+        api.voteEvent4Host(data, eventId)
+    );
+};
+
+export const useEventVote4Guest = (eventId: string) => {
+    return useMutation(["eventVote-guest"], (data:string) =>
+        api.voteEvent4Guest(data, eventId)
     );
 };
