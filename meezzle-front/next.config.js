0f86a1077df7381e71743195ce5108f35d4ea7b4
ryuni-dev <ryuni.dev@gmail.com>
2023-01-08 17:27:22 +0900

    특정 페이지 URL 직접 접근 막음

--- meezzle-front/next.config.js
+++ meezzle-front/next.config.js
@@ -2,6 +2,36 @@
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
+
+    async redirects() {
+        return [
+          {
+            source: '/login',
+            destination: '/',
+            permanent: false,
+          },
+          {
+            source: '/event/create',
+            destination: '/',
+            permanent: false,
+          },
+          {
+            source: '/event/:eid/congratulations',
+            destination: '/event/:eid/info',
+            permanent: false,
+          },
+          {
+            source: '/event/:eid/revise',
+            destination: '/event/:eid/info',
+            permanent: false,
+          },
+          {
+            source: '/event/:eid/vote',
+            destination: '/event/:eid/info',
+            permanent: false,
+          },
+        ];
+      },
 };
 
 module.exports = nextConfig;
