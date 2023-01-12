4b92c1b6b2112eba3213361bfec2036a7c19af4d
KUcodemaster <jh9485@korea.ac.kr>
2023-01-12 14:00:33 +0900

    이벤트 투표 시 선택 안된 요일 투명도 낮춤

--- meezzle-front/components/event/Vote/DayBar.tsx
+++ meezzle-front/components/event/Vote/DayBar.tsx
@@ -1,7 +1,7 @@
-import { useRecoilState } from 'recoil';
-import styled from 'styled-components';
-import { eventDaySelected } from '../../../states/eventDayBox';
-import { voteNow } from '../../../states/eventVote';
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { eventDaySelected } from "../../../states/eventDayBox";
+import { voteNow } from "../../../states/eventVote";
 
 interface Props {
     selected: boolean;
@@ -14,38 +14,40 @@ const DayContainer = styled.div`
     justify-content: center;
     align-items: center;
     max-width: 320px;
-    width: 80%
+    width: 80%;
     height: auto;
-`
+`;
 const Day = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     width: 42px;
     height: 42px;
-    color : ${(props:Props) => {
-            if(props.nowDay) {
-                return "#FFFFFF";
-            }
-            else if(props.selected) {
-                return "#3278DE";
-            }
-            else {
-                return "#97B0D6";
-            }
-        }   
-    };
-    background-color: ${(props:Props) => {
-            if(props.nowDay) {
-                return "#3278DE";
-            }
-            else {
-                return "#FFFFFF";
-            }
-        }   
-    };
+    color: ${(props: Props) => {
+        if (props.nowDay) {
+            return "#FFFFFF";
+        } else if (props.selected) {
+            return "#3278DE";
+        } else {
+            return "#97B0D6";
+        }
+    }};
+    background-color: ${(props: Props) => {
+        if (props.nowDay) {
+            return "#3278DE";
+        } else {
+            return "#FFFFFF";
+        }
+    }};
+    opacity: ${(props: Props) => {
+        if (props.nowDay || props.selected) {
+            return "1";
+        } else {
+            return "0.25";
+        }
+    }};
     border-radius: 50px;
-    font-family: 'Pretendard';
+    font-family: "Pretendard";
     font-style: normal;
     font-weight: 700;
     font-size: 20px;
@@ -53,47 +55,46 @@ const Day = styled.div`
     /* identical to box height, or 30px */
 
     letter-spacing: -0.011em;
-`
+`;
 
 const DayBar = () => {
     const [now, setNow] = useRecoilState(voteNow);
     const [selectedDay, setSelectedDay] = useRecoilState(eventDaySelected);
-    const days = ['일', '월', '화', '수', '목', '금', '토'];
+    const days = ["일", "월", "화", "수", "목", "금", "토"];
 
     const FindSelected = (idx: number): boolean => {
-        if(selectedDay.find(s => s === idx)) {
+        if (selectedDay.find((s) => s === idx)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-    }
+    };
 
     const FindNow = (idx: number): boolean => {
-        if(now === idx) {
+        if (now === idx) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-    }
+    };
     const OnClick = (e: React.MouseEvent) => {
-        try{
-            const targetElement :string = e.currentTarget.getAttribute("data-day") || '';
-            const clickDay :number = parseInt(targetElement);
-            if(selectedDay.find(s => clickDay === s)){
+        try {
+            const targetElement: string =
+                e.currentTarget.getAttribute("data-day") || "";
+            const clickDay: number = parseInt(targetElement);
+            if (selectedDay.find((s) => clickDay === s)) {
                 setNow(parseInt(targetElement));
             }
+        } catch {
+            console.log("getAtrribute Error!");
         }
-        catch{
-            console.log('getAtrribute Error!');
-        }
-    }
+    };
 
     return (
         <DayContainer>
-            {days.map((day: string, index:number) => 
-                <Day key={index + 1}
+            {days.map((day: string, index: number) => (
+                <Day
+                    key={index + 1}
                     data-day={index + 1}
                     selected={FindSelected(index + 1)}
                     nowDay={FindNow(index + 1)}
@@ -101,10 +102,9 @@ const DayBar = () => {
                 >
                     {day}
                 </Day>
-            
-            )}
+            ))}
         </DayContainer>
-    )
-}
+    );
+};
 
 export default DayBar;
