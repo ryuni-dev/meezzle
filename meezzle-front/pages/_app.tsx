5600ed318575f6e05ce740555d8cb2bd6094a9b8
ryuni-dev <ryuni.dev@gmail.com>
2023-01-12 19:58:11 +0900

    console.log 제거

--- meezzle-front/pages/_app.tsx
+++ meezzle-front/pages/_app.tsx
@@ -28,7 +28,7 @@ function MyApp({
     function kakaoInit() {
         // 페이지가 로드되면 실행
         window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-        console.log("kakao", window.Kakao.isInitialized());
+        // console.log("kakao", window.Kakao.isInitialized());
     }
 
     return (
