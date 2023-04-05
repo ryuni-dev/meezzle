7bbe2169c63db9f187898f0e1b5f2d01c81a2be6
ryuni-dev <ryuni.dev@gmail.com>
2023-04-05 11:43:58 +0900

    오타 수정

--- meezzle-front/pages/_document.tsx
+++ meezzle-front/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        /* Global Site Tag (gtag.js) - Google Analytics */}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
