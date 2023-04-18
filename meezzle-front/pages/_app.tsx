34c6310d79d96b60eddc4affd5bffce433921a31
KUcodemaster <jh9485@korea.ac.kr>
2023-04-18 22:46:24 +0900

    fix: 랜딩페이지 성능 개선 및 Header에 script 삽입된 문제 해결

--- meezzle-front/pages/_app.tsx
+++ meezzle-front/pages/_app.tsx
@@ -89,24 +89,25 @@ function MyApp({
                             type="image/x-icon"
                             href="/logo.ico"
                         />
-                         {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+                    </Head>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${GA_TRACKING_ID}', {
                 page_path: window.location.pathname,
               });
-          `
-            }}
-          />
-                    </Head>
+          `,
+                        }}
+                    />
+
                     <GlobalStyle />
                     <Component {...pageProps} />
                     <Script
